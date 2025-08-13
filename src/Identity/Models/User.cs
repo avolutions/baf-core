c6091f8d2aee6c186ef27da7407021e986a5d031
AlxndrVgt <alexander.vogt@avolutions.de>
2025-08-13 17:16:34 +0200

    feat: add more fields to user

--- src/Identity/Models/User.cs
+++ src/Identity/Models/User.cs
@@ -1,11 +1,33 @@
-﻿using Avolutions.BAF.Core.Entities.Abstractions;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Avolutions.BAF.Core.Entities.Abstractions;
 using Microsoft.AspNetCore.Identity;
 
 namespace Avolutions.BAF.Core.Identity.Models;
 
 public class User : IdentityUser<Guid>, IEntity
 {
+    public string Firstname { get; set; } = string.Empty;
+    public string Lastname { get; set; } = string.Empty;
+    public string AvatarColor { get; set; } = string.Empty;
     public string? ExternalId { get; set; }
+    [NotMapped]
+    public string RoleName { get; set; } = string.Empty;
+    public bool IsLocked()
+    {
+        return LockoutEnabled && LockoutEnd > DateTimeOffset.UtcNow;
+    }
+    public string GetName()
+    {
+        return $"{Firstname} {Lastname}";
+    }
+    public string GetInitials()
+    {
+        if (!string.IsNullOrEmpty(Firstname) && !string.IsNullOrEmpty(Lastname))
+        {
+            return $"{Firstname[0]}{Lastname[0]}".ToUpper();
+        }
+        return "";
+    }
     public DateTime CreatedAt { get; set; }
     public Guid CreatedBy { get; set; }
     public DateTime ModifiedAt { get; set; }
