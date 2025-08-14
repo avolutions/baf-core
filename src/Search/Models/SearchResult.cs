a63870651d9c8009f999f22b6471c18e96771901
AlxndrVgt <alexander.vogt@avolutions.de>
2025-08-14 10:36:42 +0200

    feat: add search

---
+++ src/Search/Models/SearchResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Avolutions.BAF.Core.Search.Models;
+
+public class SearchResult
+{
+    public string Type { get; set; } = string.Empty;
+    public string Text { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
