cd454c7a46258f5dbca19ba8586a4cef99a28c17
AlxndrVgt <alexander.vogt@avolutions.de>
2025-09-09 15:45:23 +0200

    feat: add system settings

---
+++ src/SystemSettings/Validators/SystemSettingsValidator.cs
@@ -0,0 +1,15 @@
+﻿using Avolutions.Baf.Core.SystemSettings.Resources;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Avolutions.Baf.Core.SystemSettings.Validators;
+
+public class SystemSettingsValidator : AbstractValidator<SystemSettings>
+{
+    public SystemSettingsValidator(IStringLocalizer<SystemSettingsResources> localizer)
+    {
+        RuleFor(x => x.ApplicationTitle)
+            .NotEmpty()
+            .WithName(localizer["ApplicationTitle"]);
+    }
+}
