07eed248ebdfd63bfe6a739e5bdee16f820c0789
AlxndrVgt <alexander.vogt@avolutions.de>
2025-12-30 21:19:41 +0100

    feat: add extension to register cache

---
+++ src/Caching/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Avolutions.Baf.Core.Caching.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avolutions.Baf.Core.Caching.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddCache<TInterface, TImplementation>(this IServiceCollection services)
+        where TInterface : class, ICache
+        where TImplementation : class, TInterface
+    {
+        services.AddSingleton<TImplementation>();
+        services.AddSingleton<TInterface>(sp => sp.GetRequiredService<TImplementation>());
+        services.AddSingleton<ICache>(sp => sp.GetRequiredService<TImplementation>());
+        return services;
+    }
+}
