ddd1cc38dba50cbf46a8ba61c8196bd4cbe29d70
AlxndrVgt <alexander.vogt@avolutions.de>
2026-07-29 16:29:30 +0200

    feat: add entity info

---
+++ src/Entity/Attributes/EntityResourceAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Avolutions.Baf.Core.Entity.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class EntityResourceAttribute : Attribute
+{
+    public EntityResourceAttribute(Type resourceType)
+    {
+        ResourceType = resourceType;
+    }
+
+    public Type ResourceType { get; }
+}
