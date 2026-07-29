65fecf8a39fe014f95726610ac89c902731eedce
AlxndrVgt <alexander.vogt@avolutions.de>
2026-07-29 16:28:46 +0200

    feat: make id of cache typed

--- src/Caching/Abstractions/ICache.cs
+++ src/Caching/Abstractions/ICache.cs
@@ -5,8 +5,11 @@ public interface ICache
     Task RefreshAsync(CancellationToken cancellationToken = default);
 }
 
-public interface ICache<T> : ICache
+public interface ICache<TKey, T> : ICache
+    where TKey : notnull
 {
     Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
-    Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<T?> GetByIdAsync(TKey id, CancellationToken cancellationToken = default);
 }
+
+public interface ICache<T> : ICache<Guid, T>;
