3e41e0f09ecba00ce56b5a104e7d5d670df459ef
AlxndrVgt <alexander.vogt@avolutions.de>
2026-08-21 11:44:48 +0200

    feat: add policy registrar

--- src/Identity/Extensions/ServiceCollectionExtensions.cs
+++ src/Identity/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
-﻿using Avolutions.Baf.Core.Identity.Models;
+﻿using Avolutions.Baf.Core.Identity.Abstractions;
+using Avolutions.Baf.Core.Identity.Models;
 using Avolutions.Baf.Core.Identity.Services;
+using Avolutions.Baf.Core.Module.Extensions;
 using Avolutions.Baf.Core.Persistence;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -28,6 +30,16 @@ public static class ServiceCollectionExtensions
             })
             .AddIdentityCookies();
 
+        var registrars = DiscoverPolicyRegistrars(services);
+
+        services.AddAuthorization(options =>
+        {
+            foreach (var registrar in registrars)
+            {
+                registrar.Register(options);
+            }
+        });
+        
         services.AddIdentityCore<User>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
@@ -40,5 +52,41 @@ public static class ServiceCollectionExtensions
             .AddDefaultTokenProviders();
 
         return services;
+    }
+    
+    /// <summary>
+    /// Finds all concrete types implementing <see cref="IPolicyRegistrar"/> in the
+    /// assemblies scanned by <c>AddBafCore</c> and returns the created instances.
+    /// </summary>
+    private static List<IPolicyRegistrar> DiscoverPolicyRegistrars(IServiceCollection services)
+    {
+        if (services
+                .FirstOrDefault(descriptor => descriptor.ServiceType == typeof(BafRegistry))?
+                .ImplementationInstance is not BafRegistry registry)
+        {
+            throw new InvalidOperationException(
+                "AddBafIdentity must be called after AddBafCore.");
+        }
+
+        var registrars = new List<IPolicyRegistrar>();
+
+        foreach (var assembly in registry.ScannedAssemblies)
+        {
+            var registrarTypes = assembly.GetLoadableTypes()
+                .Where(type => typeof(IPolicyRegistrar).IsAssignableFrom(type))
+                .Where(type => type is { IsAbstract: false, IsInterface: false });
+
+            foreach (var registrarType in registrarTypes)
+            {
+                if (Activator.CreateInstance(registrarType) is not IPolicyRegistrar registrar)
+                {
+                    continue;
+                }
+
+                registrars.Add(registrar);
+            }
+        }
+
+        return registrars;
     }
 }
