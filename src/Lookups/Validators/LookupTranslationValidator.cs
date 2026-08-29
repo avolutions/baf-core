137a9d59e747f45282a5373f2c37c1182694ee16
AlxndrVgt <alexander.vogt@avolutions.de>
2026-08-29 16:22:57 +0200

    feat: add base lookup translation validator

---
+++ src/Lookups/Validators/LookupTranslationValidator.cs
@@ -0,0 +1,17 @@
+﻿using Avolutions.Baf.Core.Lookups.Models;
+using Avolutions.Baf.Core.Lookups.Resources;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Avolutions.Baf.Core.Lookups.Validators;
+
+public abstract class LookupTranslationValidator<T> : AbstractValidator<T>
+    where T : LookupTranslation
+{
+    protected LookupTranslationValidator(IStringLocalizer<LookupResources> localizer)
+    {
+        RuleFor(x => x.Value)
+            .NotEmpty()
+            .WithName(localizer["Field.Value"]);
+    }
+}
